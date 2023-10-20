3be5d328a566158b37164d4ede8294e72d6240a4
Harrison Karsch <148561806+smokeykcodes@users.noreply.github.com>
2023-10-20 17:31:40 -0400

    Create rockpaperscissors.py

---
+++ rockpaperscissors.py
@@ -0,0 +1,32 @@
+import random
+
+def get_choices():
+    options = ["rock", "paper", "scissors"]
+    player_choice = input ("Enter a choice (rock, paper, or scissors): ")
+    computer_choice = random.choices (options)
+    choices = {"player" : player_choice, "computer" : computer_choice}
+    return choices
+
+def check_win(player, computer):
+    print (f"you chose {player}, computer chose {computer}")
+    if player == computer:
+        return "it's a tie!"
+    elif player == "rock":
+        if computer == "scissors":
+            return "rock smashes scissors! YOU WIN!"
+        else:
+            return "YOU LOSE"
+    elif player == "paper":
+        if computer == "rock":
+            return "paper covers rock! YOU WIN!"
+        else:
+            return "YOU LOSE"
+    elif player == "scissors":
+        if computer == "paper":
+            return "Scissors cuts paper! YOU WIN!"
+        else:
+            return "YOU LOSE"
+
+choices = get_choices()
+result = check_win (choices ["player"], choices ["computer"])
+print (result)
